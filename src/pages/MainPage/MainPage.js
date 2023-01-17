41efb542a867935b9bf0bfc6b2044b162d504d4d
Zarina1708 <115084290+Zarina1708@users.noreply.github.com>
2023-01-18 00:39:31 +0600

    Update MainPage.js

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,17 +1,12 @@
-import { useEffect } from "react"
-import { useState } from "react"
+import {useEffect} from "react"
+import {useState} from "react"
 import User from "../../components/user/User"
 
 
-
-
-
-export default function MainPage () {
+export default function MainPage() {
 
     const [users, setUsers] = useState([])
 
-      
-    
 
 
     useEffect(() => {
@@ -19,34 +14,44 @@ export default function MainPage () {
             .then(response => {
                 if (response.status === 200) {
                     return response.json()
-                }else {
+                } else {
                     throw response.status
                 }
             })
             .then(data => setUsers(data))
-            .then(error => console.log(error))
-    },[])
+            .catch(error => console.log(error))
+    }, [])
 
+
+    const [more, setMore] = useState('')
+
+    const {name, username, address, email} = more
 
     return (
         <>
             <h1> Main Page</h1>
             <div style={{display: 'flex'}}>
                 <div>
-                {
-                    users.map(user => <User userInfo={user}/>)
-                }  
+                    {
+                        users.map((item) => (
+                            <div>
+                                <User key={item.id} userInfo={item}/>
+                                <button onClick={() => setMore(item)}>подробнее</button>
+                            </div>
+                        ))
+                    }
                 </div>
-                
 
-                {/* <ul>
-                    <li>name: </li>
-                    <li>username: </li>
-                    <li>email: </li>
-                    <li>city: </li>
-                </ul> */}
+
+                <ul>
+                    <li>name: {name}</li>
+                    <li>username: {username}</li>
+                    <li>email: {email}</li>
+                    <li>city: {address?.city}</li>
+                </ul>
+
             </div>
-        
+
         </>
     )
 }
