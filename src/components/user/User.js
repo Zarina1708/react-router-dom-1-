70d3dbe227c7f88681ca4b524726eddf721d714c
Zarina1708 <115084290+Zarina1708@users.noreply.github.com>
2023-01-18 00:38:04 +0600

    Update User.js

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,39 +1,15 @@
-import { useState } from "react"
+import {useState} from "react";
 
 
+export default function User({userInfo}) {
 
-
-export default function User ({userInfo}) {
-
-    const [user, setUser] = useState({})
-
-    function getUser(){
-        fetch(`https://jsonplaceholder.typicode.com/users/${userInfo.id}`)
-        .then(response => response.json())
-        .then(data => setUser(data))
-    }
-  
-
+    // console.log(userInfo)
 
     return (
         <div>
-        
-        <h3>{userInfo.name}</h3>
 
-        {
-            user && (
-                <ul>
-                    <li>name: {user.name}</li>
-                    <li>username: {user.username}</li>
-                    <li>email: {user.email}</li>
-                    <li>city: {user?.address?.city}</li>
-                </ul>
-            )
-        }
-        <button value={userInfo.id} onClick={getUser} >подробнее</button>
-
-     
-        <p>-----------------</p>
+            <h3>{userInfo.name}</h3>
+            <p>-----------------</p>
 
         </div>
     )
